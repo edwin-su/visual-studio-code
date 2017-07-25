41df3e99d98d14eac68742895f6234fcc4935da6
edwinsu <edwinsu@augmentum.com.cn>
2017-07-25 17:48:21 +0800

    http://blog.csdn.net/yym373872996/article/details/52927731?locationNum=7&fps=1

---
+++ Entity Framework Code First Example/Entity Framework Code First Example/DAL/SchoolContext.cs
@@ -0,0 +1,28 @@
+﻿using Entity_Framework_Code_First_Example.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+namespace Entity_Framework_Code_First_Example.DAL
+{
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext() : base("SchoolContext")  // The SchoolContext is a connection string which will be defined in web.config. If you don't declare this, it will using the class name.
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+
+        public DbSet<Enrollment> Enrollments { get; set; }  // This can be omitted. Because enrollment is using by student.
+
+        public DbSet<Course> Courses { get; set; }  // This can be omitted. Because course is using by enrollment.
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder) // This can prevent 's' in name. Because we need the table name as Student, Enrollment, Course. Not like the Students above.
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
---
+++ Entity Framework Code First Example/Entity Framework Code First Example/Models/Enrollment.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Entity_Framework_Code_First_Example.Models
+{
+    public enum Grade
+    {
+        A, B, C, D, F
+    }
+
+    public class Enrollment
+    {
+        // This primary key is just an example. The application should follow the same rule. Using Id as the primary key or using the class name + id as the primary key.
+        public int EnrollmentID { get; set; } 
+
+        public int CourseID { get; set; }
+
+        public int StudentID { get; set; }
+
+        public Grade? Grade { get; set; }
+
+        public virtual Course Course { get; set; }
+
+        public virtual Student Student { get; set; }
+    }
+}
